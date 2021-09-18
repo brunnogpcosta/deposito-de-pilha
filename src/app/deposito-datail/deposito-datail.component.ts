6e6cbb5ff287169964b777cdeeda515814c4d5fe
BrunnoCostaUNI <brunno.costa@unigranrio.br>
2021-09-18 18:28:32 -0300

    html e css pagina de detalhes

--- src/app/deposito-datail/deposito-datail.component.ts
+++ src/app/deposito-datail/deposito-datail.component.ts
@@ -3,13 +3,12 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-deposito-datail',
   templateUrl: './deposito-datail.component.html',
-  styleUrls: ['./deposito-datail.component.css']
+  styleUrls: ['./deposito-datail.component.css'],
 })
 export class DepositoDatailComponent implements OnInit {
+  deposito: any = {};
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
