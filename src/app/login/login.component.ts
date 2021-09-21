0ea3f696486224ee10466abcbef2759e7d4b6720
BrunnoCostaUNI <brunno.costa@unigranrio.br>
2021-09-21 17:37:57 -0300

    capturando as informações de login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -3,13 +3,16 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+  login: any = {};
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  validaLogin() {
+    console.table({ login: this.login.email, senha: this.login.senha });
   }
-
 }
